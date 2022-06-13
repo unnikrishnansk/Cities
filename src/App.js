547bedeba54e9d10299a5d9e2cbff269a136f7a4
unnikrishnansk <unni.sk94@gmail.com>
2022-06-13 22:38:13 +0530

    commit citis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Cities from './components/Cities';
+import Countries from './components/Countries';
+import { useEffect , useState } from 'react';
 
 function App() {
+
+  const [countries, setcountries] = useState([]);
+  const [name, setname] = useState("");
+
+  const getcountries = () => {
+    fetch(`https://62a6c393bedc4ca6d7b992eb.mockapi.io/countries`)
+    .then((res) => res.json())
+    .then((data) => {
+      setcountries(data);
+    })
+  }
+
+  useEffect(() => {
+    getcountries();
+  }, [])
+
+  const addcountry = () => {
+    fetch(`https://62a6c393bedc4ca6d7b992eb.mockapi.io/countries`,{
+      method : "POST",
+      headers : {
+        "content-type" : "application/json",
+      },
+      body : JSON.stringify({
+        data : countries,
+      }),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+    setname([...countries,data])
+    setname("");
+    });
+  }
+
+//  console.log(countries);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Countries />
+     <select>
+     {countries.map((country) => (
+      <option value={country.name} key={country.id}>{country.name}</option> 
+     ))}
+     </select>
+
+     <div>
+      <input placeholder='enter a city name to add' value={name} onChange={(e) => {
+        console.log(e.target.value);
+        setname(e.target.value)}} />
+      <button onClick={addcountry}>ADD</button>
+
+      <Cities /> 
+      <input placeholder='Enter the city name'/>
+      <input placeholder='Enter population'/>
+      <div>
+      <select>
+      {countries.map((country) => (
+      <option value={country.name} key={country.id}>{country.name}</option> 
+     ))}
+     </select>
+     </div>
+
+     </div>
     </div>
+    
   );
 }
 
